18f4193a521545ffe4721297c5a8409cdc64f4ae
Wesley Ijams <wijams@byu.edu>
2022-03-16 15:55:27 -0600

    Commiting

--- back-end/server.js
+++ back-end/server.js
@@ -14,5 +14,80 @@ mongoose.connect('mongodb://localhost:27017/museum', {
   useNewUrlParser: true
 });
 
+app.post('/api/items', async (req, res) => {
+    const item = new Item({
+        title: req.body.title,
+        description: req.body.description,
+        path: req.body.path,
+    });
+    try {
+        await item.save();
+        res.send(item);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+const multer = require('multer')
+const upload = multer({
+    dest: '../front-end/public/images/',
+    limits: {
+        fileSize: 10000000
+    }
+});
+
+const itemSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    path: String,
+});
+
+const Item = mongoose.model('Item', itemSchema);
+
+app.post('/api/photos', upload.single('photo'), async (req, res) => {
+    if (!req.file) {
+        return res.sendStatus(400);
+    }
+    res.send({
+        path: "/images/" + req.file.filename
+    });
+});
+
+app.get('/api/items', async (req, res) => {
+    try {
+        let items = await Item.find();
+        res.send(items);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.delete('/api/items/:id', async (req, res) => {
+    try {
+        await Item.deleteOne({
+            _id: req.params.id
+        });
+        res.sendStatus(200);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+});
+
+app.put('/api/items/:id', async (req, res) => {
+    try {
+        let item = await Item.findOne({
+            _id: req.params.id
+        });
+        item.title = req.body.title;
+        item.description = req.body.description;
+        await item.save();
+        res.send(item);
+    } catch (error) {
+        console.log(error);
+        res.sendStatus(500);
+    }
+})
 
 app.listen(3000, () => console.log('Server listening on port 3000!'));
